481a248ed7a47265536135bec93d87db1bf842ea
nebojsa.marjanovic <nebojsa.marjanovic@teletrader.com>
2023-10-28 17:57:29 +0200

    Add project files.

---
+++ AuthenticationExample/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationExample
+{
+    public class DataContext:IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=AuthenticationDB; Trusted_Connection=True;");
+        }
+    }
+}
